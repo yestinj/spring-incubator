d9864f73de591324e68bdd912d8cc499ad23b2ac
Yestin Johnson <1382137+yestinj@users.noreply.github.com>
2025-07-04 16:39:00 +0200

    Improvements to security config for loyalty service to try and get authentication working. In progress

---
+++ loyalty-service/src/main/java/entelect/training/incubator/spring/loyalty/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package entelect.training.incubator.spring.loyalty.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    InMemoryUserDetailsManager inMemoryAuthManager() {
+        return new InMemoryUserDetailsManager(
+                User.builder().username("user").password("{noop}the_cake").roles("USER").build(),
+                User.builder().username("admin").password("{noop}is_a_lie").roles("ADMIN").build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(authorize -> authorize
+//                        // we really shouldn't do this, but this was the only way I could find
+//                        // to get requests to actually work
+//                        .anyRequest().permitAll()
+//                )
+//                .httpBasic(AbstractHttpConfigurer::disable);
+//
+//        return http.build();
+        http.csrf().disable() // !!! Disclaimer: NEVER DISABLE CSRF IN PRODUCTION !!!
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/ws/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/ws/**").hasAnyRole("SYSTEM", "ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
